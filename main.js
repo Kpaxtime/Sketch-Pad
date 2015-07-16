8db2a79c61c140f21daa60ca95c0156bc7c7b546
Kpaxtime <twyrick94@gmail.com>
2015-07-16 17:50:27 -0500

    Added Framework for grid functionality

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
 
-	$('#grid-holder').hover(function () {
-		$(this).css('background-color', 'red');
-	});
+	blockSetup();
 
+});
+
+
+function blockSetup() {
+$('option').on('click', function() {
+	var size = $('option').data('dimension');
+	if(size === 8) {
+		$('.data-block').addClass('.size8');
+	}
+	else if(size === 16) {
+		$('.data-block').addClass('.size16');
+	}
+	else if(size === 32) {
+		$('.data-block').addClass('.size32');
+	}
+	else {
+		$('.data-block').addClass('.size64');
+	}
+
+	for(var i = 0; i < size, i++) {
+		$('#grid-holder').append('.data-block');
+	}
+}
 });
