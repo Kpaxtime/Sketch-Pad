7b6c7f6a545a1a5c24664347b4fb2adda68b38a2
Kpaxtime <twyrick94@gmail.com>
2015-07-17 17:05:31 -0500

    Added trail and clearing functionality

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ $(document).ready(function () {
 });
 	initialize();
 	gradient();
+
 	
+	$('#gradient').on('click', function () {
+		cleargrid();
+	});
+	
+
+	$('#trail').on('click', function() {
+		cleargrid();
+		trail();
+	});
 });
 
 
@@ -47,3 +57,33 @@ function gradient() {
 		$(this).css("opacity", currentOpacity - 0.30);
 	});
 }
+
+function trail() {
+	$('.data-block').on('mouseenter', function() {
+		$(this).css("opacity", 0);
+		$(this).animate({opacity: 1}, "slow");
+	});
+}
+
+function randomColor() {
+	$('.data-block').on('mouseenter',function() {
+		var maxNum = 16777215;
+
+		var newColor = Math.ceil((maxNum * Math.random())).toString(16);
+		var colorLength  = 6 - newColor.length;
+
+		for(var i = 0; i < testLength; i++) {
+			newColor = "0" + newColor;
+		}
+
+		$(this).css('background-color', '#' + testColor);
+	});
+}
+
+function cleargrid() {
+	$('button').on('click', function() {
+		$('#grid-holder').empty();
+		initialize();
+		gradient();
+	});
+}	
